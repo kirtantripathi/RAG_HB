34f122bc3e68945f15c00212c4b9e58d7d6e19ab
tripathi kirtan <tripathikirtan9@gmail.com>
2025-11-28 09:08:26 +0530

    1 - Data gathered, Basic indexing done, Basic Backend done

---
+++ app.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+import json
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
+from fastapi.responses import JSONResponse
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+
+key = open("keys.txt").read()
+embed_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+# db = FAISS.load_local("faiss_index", embed_model, allow_dangerous_deserialization=True)
+model = ChatGroq(model="llama-3.1-8b-instant", temperature=0.1, api_key=key)
+
+prompt_template = PromptTemplate(template="""You are an AI Assistant for Sunridge Institute of Technology (SIT).
+Your job is to answer student queries using ONLY the information provided in the retrieved context. If Query can not be answered from provided context just say i don't know.
+
+CONTEXT:
+{context}
+
+QUERY:
+{query}
+""", input_variables=["context","query"])
+
+db = FAISS.load_local(
+    folder_path="faiss_index",
+    embeddings=embed_model,
+    index_name="index",
+    allow_dangerous_deserialization=True
+)
+
+app = FastAPI()
+
+class Query(BaseModel):
+    message: str
+
+@app.get("/")
+def home():
+    return {"message":"welcome vats!!"}
+
+@app.post("/ask")
+def ask(request: Query):
+    results = db.similarity_search(request.message, k=3)
+    print(results)
+    formatted = [
+        {
+            "content": doc.page_content,
+            "metadata": doc.metadata
+        }
+        for doc in results
+    ]
+    p = prompt_template.invoke({"context":results,"query":request.message})
+    response = model.invoke(p)
+    return JSONResponse(content={"results": response.content})
---
+++ data_loading.py
@@ -0,0 +1,63 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+import time
+import streamlit as st
+from langchain_core.documents import Document
+
+
+def load_document(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        text = f.read()
+
+    file_name = os.path.basename(file_path).replace(".txt","")
+    return Document(page_content=text,metadata={"source": file_name})
+    
+
+def load_documents(folder_path):
+    documents = []
+    for i in os.walk(folder_path):
+        for j in i[2]:
+            documents.append(load_document(os.path.join(i[0],j)))
+            print(os.path.join(i[0],j))
+    return documents
+
+
+def chunk_documents(documents):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        separators=["\n\n", "\n", ".", "!", "?", ",", " "]
+    )
+    return splitter.split_documents(documents)
+
+
+def generate_embeddings():
+    return HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+def build_and_save_vectorstore(chunks, embed_model, save_path="faiss_index"):
+    vector_db = FAISS.from_documents(chunks, embed_model)
+    vector_db.save_local(save_path)
+    print(f"Vectorstore saved at: {save_path}")
+
+
+def load_data(folder_path):
+    print("Loading documents...")
+    documents = load_documents(folder_path)
+
+    print("Splitting into chunks per document...")
+    splitted_documents = chunk_documents(documents)
+    print(f"\n\n\nTotal chunks created: {len(splitted_documents)}")
+
+    print("Generating embeddings...")
+    embed_model = generate_embeddings()
+
+    print("Saving FAISS vectorstore...")
+    build_and_save_vectorstore(splitted_documents, embed_model)
+
+load_data("Data")
